b5a5f1842b6a293ea9962aa9df896a4b0bcf9853
AllanSilva29 <allanjulio61@gmail.com>
2023-12-16 15:16:28 -0300

    [ADD] os.system('clear') para limpar tela

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from character import Hero, Enemy
 from weapon import short_bow, iron_sword, bazuuka
 
@@ -8,6 +9,7 @@ enemy = Enemy(name="Enemy", health=100, weapon=short_bow)
 bazuuka_uses = 1
 
 while True:
+    os.system("clear")
     hero.attack(enemy)
     enemy.attack(hero)
 
@@ -17,11 +19,12 @@ while True:
     hero.health_bar.draw()
     enemy.health_bar.draw()
 
-    input()
-
     if bazuuka_uses >= 1: 
         hero.equip(bazuuka)
         bazuuka_uses -= 1
         print(f"Using bazuuka! Uses left: {bazuuka_uses}")
     else: 
         hero.drop()
+    
+    input()
+    
